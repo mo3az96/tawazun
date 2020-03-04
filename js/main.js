ccb5ec174ce243e33cf79fef0a0e93edf0f130dc
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-03-04 15:57:09 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(window).on("load", function () {
+    $(".loader").fadeOut(500, function () {
+        $('body').css("overflow", "visible");
+        $('body').animate({
+            scrollTop: 0
+        }, 1);
+    });
+});
+$(document).ready(function () {
+    new WOW().init();
+    $('.menubtn').click(function () {
+        $("nav").fadeToggle("500")
+        $(".quotation").toggleClass("open")
+        $(".page-content").toggleClass("open")
+        $(this).toggleClass('open');
+    });
+});
