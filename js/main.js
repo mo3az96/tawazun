efa84447aa6e5f72349b228bd5728a687f6113db
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-03-05 11:36:05 +0200

    menu

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ $(document).ready(function () {
     new WOW().init();
     $('.menubtn').click(function () {
         $("nav").fadeToggle("500")
+        $("nav").toggleClass("open")
         $(".quotation").toggleClass("open")
         $(".page-content").toggleClass("open")
         $(this).toggleClass('open');
