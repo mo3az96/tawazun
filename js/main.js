e922ff1f2c3e554180a472954acf120d3add8902
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-03-08 18:18:03 +0200

    sayed's edits 1

--- js/main.js
+++ js/main.js
@@ -13,6 +13,17 @@ $(document).ready(function () {
             scrollTop: $("#topBtnRef").offset().top - 121
         }, 2000);
     });
+    $('a[data-scroll]').click(function (e) {
+        e.preventDefault();
+        //Set Offset Distance from top to account for fixed nav
+        var offset = 70;
+        var target = ('#' + $(this).data('scroll'));
+        var $target = $(target);
+        //Animate the scroll to, include easing lib if you want more fancypants easings
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 121
+        }, 1000, 'swing');
+    });
     $('.menubtn').click(function () {
         $("nav").fadeToggle("500")
         $("nav").toggleClass("open")
@@ -42,5 +53,42 @@ $(document).ready(function () {
         } else {
             $("header.single-pro").removeClass("fixed");
         }
+    });
+});
+
+
+$(function () {
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, else we gonna set it if we can.
+            if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
     });
 });
